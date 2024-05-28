50f3cc6f1aecb6cf385a2f3b04125b81dc9271a8
Luiz Rech <luizadre@hotmail.com>
2024-05-28 10:56:45 +0100

    Finish Sidebar

---
+++ src/app/components/Input.tsx
@@ -0,0 +1,31 @@
+import { ComponentProps } from 'react'
+
+type InputPrefixProps = ComponentProps<'div'>
+
+export function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+
+type InputControlProps = ComponentProps<'input'>
+
+export function Control(props: InputControlProps) {
+  return (
+    <input
+      type="text"
+      placeholder="Search"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+
+type InputRootProps = ComponentProps<'div'>
+
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="max-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}
